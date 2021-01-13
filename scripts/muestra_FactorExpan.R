847b8fe66df535cada4c9dc442faf6f867594a0e
Cinthia Rosales <44278812+CinthiaRosS12@users.noreply.github.com>
2021-01-12 22:55:35 -0600

    Add files via upload
    Código para selección de una muestra bajo MAS y cálculo de Factores de expansión

---
+++ scripts/muestra_FactorExpan.R
@@ -0,0 +1,69 @@
+library(foreign)
+#library(tidyverse)
+library(dplyr) #filtrar datos, usar %>%
+library(tidyr) #replace_na
+library(naniar) #replace_na_with_if
+library(sampling) #MAS
+
+setwd("D:/CINTHIA/DIAO/ProyectoCovid/09")
+mzn_viv<- read.dbf("tablas/cpv2010_manzanas_viviendas.dbf") 
+mzn_pob<-read.dbf("manzanas.dbf")
+View(mzn_viv) 
+View(mzn_pob)
+
+
+#viv2
+#CTRL + SHIFT + M -> %>%
+marco <- mzn_viv %>% select(CVEGEO, VIV2) %>% 
+  left_join(mzn_pob, by = "CVEGEO") %>% 
+  mutate(ID_unit = 1:nrow(mzn_viv)) %>% 
+  select(ID_unit, CVEGEO, VIV2, POB20, POB51, POB76) %>% 
+  naniar::replace_with_na_if(.predicate = is.numeric, condition = ~.x < 0)
+
+
+
+#viv2 viviendas particulares habitadas
+#pob20 pob de 15 y más
+
+#srswor MAS sin remplazo
+#srswr MAS con remplazo
+#systematic proporcional al tamaño
+
+
+set.seed(310308261)
+muestra <- sampling::strata(marco, stratanames = NULL, size = 20, method = "srswor", description = TRUE)
+
+#fe_mzn: factor de expansion
+muestra_ponde <- marco %>% 
+  filter(ID_unit %in% muestra$ID_unit) %>% 
+  left_join(muestra, by = "ID_unit") %>% 
+  mutate(fe_mzn = 1/Prob)
+
+#Estimación del numero de viviendas con base en una muestra de 20 manzanas
+muestra_ponde %>% 
+  summarise(Total = sum(fe_mzn), 
+            pob_total = sum(fe_mzn*POB20),
+            viviendas = sum(fe_mzn * VIV2))
+
+# valores reales
+marco %>% 
+  summarise(viviendas = sum(VIV2),
+            pob_total = sum(POB20, na.rm = TRUE))
+
+#error relativo viviendas
+dif = (2378763 - 2260794) / 2378763
+
+#error relativo poblacion
+dif2 = (6640095 - 6634328) / 6640095
+
+
+#####################
+
+muestra2 <- sampling::strata(marco, stratanames = NULL, size = 20, method = "systematic", pik = marco$VIV2, description = TRUE)
+
+#fe_mzn: factor de expansion
+muestra_ponde <- marco %>% 
+  filter(ID_unit %in% muestra2$ID_unit) %>% 
+  left_join(muestra2, by = "ID_unit") %>% 
+  mutate(fe_mzn = 1/Prob)
+

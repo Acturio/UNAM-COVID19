cbd268b2f01fcf0cc3c6a5adce1b824b1b65a1e1
Arturo <arturo.bringas@sinnia.com>
2021-01-12 22:09:29 -0600

    creación de marco y selección de muestra MAS y PPT

---
+++ scripts/sesion_12012021.R
@@ -0,0 +1,4 @@
+# Sesión 3 martes 12/Ene/2021:  
+# Revalizamos un primer ejemplo para crear un marco de muestreo y crear 2 muestras
+# La primer muestra se realiza con MAS
+# La segunda muestra se realiza con PPT

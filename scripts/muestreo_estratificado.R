55c64ea207209567f1572831b982d20ba7fa53f1
AnaTorresR <anaetorresr@ciencias.unam.mx>
2021-01-18 21:35:59 -0600

    Clase 18 enero

--- scripts/muestreo_estratificado.R
+++ scripts/muestreo_estratificado.R
@@ -91,14 +91,101 @@ afijacion_proporcional <- marco_estratificado %>%
                           group_by(id_estrato) %>% 
                           summarise(viv_total = sum(VIV2), .groups = "drop") %>% 
                           mutate(prop_viv = viv_total/sum(viv_total),
-                                 n_h = round(prop_viv * 2500, 0)) %>% 
-                          pull(n_h)
+                                 n_h = round(prop_viv * 2500, 0))
                       
 muestra_estratificada2 <- sampling::strata(marco_estratificado, stratanames = "id_estrato",
-                                          size = afijacion_proporcional, method = "systematic", 
-                                          pik = marco_estratificado$VIV2, description = TRUE)
+                                          size = afijacion_proporcional %>% pull(n_h),
+                                          method = "systematic", 
+                                          pik = marco_estratificado$VIV2,
+                                          description = TRUE)
 
 muestra_estratificada_prob_ppt <- marco_estratificado %>% 
   filter(ID_unit %in% muestra_estratificada2$ID_unit) %>% 
   left_join(muestra_estratificada2, by = "ID_unit") %>% 
   mutate(fe_mzn = 1/Prob)
+
+
+########################################### LUNES 18 ENERO #############################################
+
+head(marco_estratificado)
+
+marco_estratificado_pik<- marco_estratificado %>% 
+  left_join(select(afijacion_proporcional, id_estrato, n_h), by = "id_estrato") %>% 
+  mutate(pik = inclusionprobastrata(id_estrato,afijacion_proporcional %>% pull(n_h))) %>% 
+  group_by(id_estrato) %>% 
+  mutate(pik_2 = VIV2/sum(VIV2)*n_h)
+
+#Corroborar pik_2
+marco_estratificado_pik %>% 
+  group_by(id_estrato) %>% 
+  summarise(viv2 = sum(VIV2))
+
+# group_split 
+marco_estratificado_pik %>% 
+  group_split(id_estrato)
+
+#Muestra con base a la afijación proporcional 
+muestra_estratificada_pik <- sampling::strata(marco_estratificado_pik,
+                                              stratanames = "id_estrato",
+                                              size = afijacion_proporcional %>% pull(n_h),
+                                              method = "systematic", 
+                                              pik = marco_estratificado_pik$pik, description = TRUE)
+
+
+################################# Muestreo poli etápico ###########################################
+
+############
+## Example 1
+############
+# Two-stage cluster sampling
+# Uses the 'swissmunicipalities' data 
+data(swissmunicipalities)
+b=swissmunicipalities
+b=b[order(b$REG,b$CT),]
+attach(b)
+# the variable 'REG' (region) has 7 categories;
+# it is used as clustering variable in the first-stage sample
+# the variable 'CT' (canton) has 26 categories; 
+# it is used as clustering variable in the second-stage sample
+# 4 clusters (regions) are selected in the first-stage 
+# 1 canton is selected in the second-stage from each sampled region 
+# the method is simple random sampling without replacement in each stage
+# (equal probability, without replacement)
+m=mstage(b,stage=list("cluster","cluster"), varnames=list("REG","CT"),
+         size=list(4,c(1,1,1,1)), method=list("srswor","srswor"), description = TRUE)
+# the first stage is m[[1]], the second stage is m[[2]]
+#the selected regions
+unique(m[[1]]$REG)
+#the selected cantons
+unique(m[[2]]$CT)
+# extracts the observed data
+x=getdata(b,m)[[2]]
+# check the output
+table(x$REG,x$CT)
+
+
+############
+## Example 3
+############
+# Stratified one-stage cluster sampling
+# The same data as in Example 2
+# the variable 'state' is used as stratification variable 
+# 165 units are in the first stratum and 70 in the second one
+# the variable 'region' is used as clustering variable
+# 1 cluster (region) is drawn in each state using "srswor" 
+data=rbind(matrix(rep('n',165),165,1,byrow=TRUE),matrix(rep('s',70),70,1,byrow=TRUE))
+data=cbind.data.frame(data,c(rep('A',115),rep('D',10),rep('E',40),rep('B',30),rep('C',40)),
+                      100*runif(235))
+names(data)=c("state","region","income")
+data=data[order(data$state,data$region),]
+
+m=mstage(data, stage=list("stratified","cluster"), varnames=list("state","region"), 
+         size=list(c(165,70),c(1,1)),method=list("","srswor"), description = TRUE) 
+# check the first stage
+table(m[[1]]$state)
+# check the second stage
+table(m[[2]]$region)
+# extracts the observed data
+xx=getdata(data,m)[[2]]
+# check the result
+table(xx$state,xx$region)
